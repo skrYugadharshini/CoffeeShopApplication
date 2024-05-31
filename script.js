6eb9655afac63fa721b45405a2a49f5f11b7c138
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-05-31 19:48:17 +0530

    basic

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
+
+let cartItem = document.querySelector('.cart-item-container');
+
+let cartItem = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+let cartItem = document.querySelector('.cart-item-containers');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+}
